7049a269bd8d97abc8654cdb7073904b754c1e53
MikkieR <maria.drozdova@polytechnique.edu>
2018-12-03 20:12:31 +0300

    update

--- create3Dbase.py
+++ create3Dbase.py
@@ -3,9 +3,16 @@ import os
 from testSilhouettes import *
 from randomSphereVectors import *
 
-d = 5
-
-models_list = os.listdir("models/")
+d = 20
 centers0, directions0 = determineViewDirections(d)
-for iteration0 in range(d):
-	execute("models/"+models_list[136], centers0, directions0, iteration0)
+iteration0 = 0
+execute("models/m913.off", centers0, directions0, iteration0, ["models/m913.off"])
+"""
+models_list = os.listdir("models/")[163:164]
+for i in range(len(models_list)):
+	models_list[i] = 'models/' + models_list[i]
+centers0, directions0 = determineViewDirections(d)
+for i in range(len(models_list)):
+	for iteration0 in range(d):
+		execute(models_list[i], centers0, directions0, iteration0, models_list)
+"""
--- testSilhouettes.py
+++ testSilhouettes.py
@@ -45,13 +45,15 @@ def init():
     #lightpos = (-50*up[0]*(-center[0] + eye[0]), -50*up[1]*(-center[1]+ eye[0]), -50*up[2]*(-center[2]+ eye[0]))          # Положение источника освещения по осям xyz
 
 
-    glClearColor(1.0, 1.0, 1.0, 1.0)                # Серый цвет для первоначальной закраски
-    gluOrtho2D(-1.0, 1.0, -1.0, 1.0)                # Определяем границы рисования по горизонтали и вертикали
+    glClearColor(1.0, 1.0, 1.0, 1.0)                # Gray color of thr background
+    gluOrtho2D(-1.0, 1.0, -1.0, 1.0)                # The borders for painting
     
    
-    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambient) # Определяем текущую модель освещения
-    glEnable(GL_LIGHTING)                           # Включаем освещение
-    glEnable(GL_LIGHT0)                             # Включаем один источник света
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambient) # Model of light
+    glEnable(GL_LIGHTING)                           # Turn on all the lights
+    glEnable(GL_LIGHT0)                             # Turn on the concret light
+    glEnable(GL_DEPTH_TEST)
+    glEnable(GL_CULL_FACE)
     
 
 
@@ -61,148 +63,169 @@ def draw():
     global ambient      # Рассеянное освещение
     global greencolor   # Цвет елочных иголок
     global treecolor    # Цвет елочного ствола
-    global lightpos     # Положение источника освещения 
+    global lightpos     # Position of lights
     global filename
     global centers
     global directions
     global iteration
+    global names
 
 
-                                  # Очищаем экран и заливаем серым цветом
-    #glPushMatrix()                                              # Сохраняем текущее положение "камеры"
-    verticies, surfaces = read_off(open(filename))#73
-    eye = [0., 0., 0.] 
-    glClear(GL_COLOR_BUFFER_BIT)
-    for iteration in range(len(centers)):
-        glClear(GL_COLOR_BUFFER_BIT)  
-        center = centers[iteration]
-        up = directions[iteration]
-        lightpos = (5000*center[0], 5000*center[1], 5000*center[2])          # Положение источника освещения по осям xyz
-        glLoadIdentity()
-        gluLookAt(eye[0] , eye[1], eye[2], center[0], center[1], center[2], up[0], up[1], up[2])
-        glLightfv(GL_LIGHT0, GL_POSITION, lightpos)     # Определяем положение источника света
-        res = [0,0,0]
-        for vert in verticies:
-            res = sum(vert, res)
-        res = div(res, len(verticies))
-        for n_vert in range(len(verticies)):
-            verticies[n_vert] = sum(verticies[n_vert], opp(res))
-        glBegin(GL_TRIANGLES)
-        #glShadeModel(GL_FLAT)
-        x = 0
-        normals = []
-        n = len(verticies)
-        vertices_to_surfaces = []
-        for i in range(n):
-            vertices_to_surfaces.append([])
-        for surface in surfaces:
-            if len(surface) == 0:
+    for filename in names:    
+        print(filename)
+        verticies, surfaces = read_off(open(filename))#73
+        eye = [0., 0., 0.] 
+        for iteration in range(len(centers)):
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            
+            #
+            center = centers[iteration]
+            up = directions[iteration]
+            lightpos = (5000*center[0], 5000*center[1], 5000*center[2])  
+            glLoadIdentity()
+            glEnable(GL_DEPTH_TEST);
+            gluLookAt(eye[0] , eye[1], eye[2], 20*center[0], 20*center[1], 20*center[2], up[0], up[1], up[2])
+
+            glLightfv(GL_LIGHT0, GL_POSITION, lightpos)     # where are the lights
+            #glEnable(GL_CULL_FACE)
+
+
+            res = [0,0,0]
+            for vert in verticies:
+                res = sum(vert, res)
+            res = div(res, len(verticies))
+            for n_vert in range(len(verticies)):
+                verticies[n_vert] = sum(verticies[n_vert], opp(res))
+            #glBegin(GL_TRIANGLES)
+            #glShadeModel(GL_FLAT)
+            x = 0
+            normals = []
+            n = len(verticies)
+            #line 5: 1, 5, 8(order like in surfaces)
+            vertices_to_surfaces = []
+            for i in range(n):
+                vertices_to_surfaces.append([])
+            for surface in surfaces:
+                if len(surface) == 0:
+                    #print(surface)
+                    continue
+                    print('2')
+                x = x + 1
+                u = [verticies[surface[1]][0]-verticies[surface[0]][0], verticies[surface[1]][1]-verticies[surface[0]][1], verticies[surface[1]][2]-verticies[surface[0]][2]]
+                v = [verticies[surface[2]][0]-verticies[surface[0]][0], verticies[surface[2]][1]-verticies[surface[0]][1], verticies[surface[2]][2]-verticies[surface[0]][2]]
+                normal= [u[1]*v[2] - u[2]*v[1], u[2]*v[0] - u[0]*v[2], u[0]*v[1]-u[1]*v[0]]
+
+                # une tentative a comprendre si dehors ou interieurs
+                s = 0
+                for i in range(3):
+                    s = s+normal[i]*verticies[surface[0]][i]
+                if True:
+                    normals.append(normal)
+                else:
+                    normals.append([-normal[0], -normal[1], -normal[2]])
+                    #print('inverse')
+                for vertex in surface:
+                    vertices_to_surfaces[vertex].append(x-1)  	
+            x = 0
+            vertex_normalized = []
+            #normals for each vertice = average of all normals of the adjoint vertices
+            for j in range(len(vertices_to_surfaces)):
+                vertex_normalized.append([0, 0, 0])
+                for z in range(len(vertices_to_surfaces[j])):
+                    normal = normals[vertices_to_surfaces[j][z]]
+                    vertex_normalized[j][0] = vertex_normalized[j][0] + normal[0]
+                    vertex_normalized[j][1] = vertex_normalized[j][1] + normal[1]
+                    vertex_normalized[j][2] = vertex_normalized[j][2] + normal[2]
+                norm = np.sqrt(vertex_normalized[j][0]**2 + vertex_normalized[j][1]**2 + vertex_normalized[j][2]**2)
+                for z in range(3):
+                    vertex_normalized[j][z] = vertex_normalized[j][z]/norm
+
+
+            for surface in surfaces:
                 #print(surface)
-                continue
-                print('2')
-            x = x + 1
-            u = [verticies[surface[1]][0]-verticies[surface[0]][0], verticies[surface[1]][1]-verticies[surface[0]][1], verticies[surface[1]][2]-verticies[surface[0]][2]]
-            v = [verticies[surface[2]][0]-verticies[surface[0]][0], verticies[surface[2]][1]-verticies[surface[0]][1], verticies[surface[2]][2]-verticies[surface[0]][2]]
-            normal= [u[1]*v[2] - u[2]*v[1], u[2]*v[0] - u[0]*v[2], u[0]*v[1]-u[1]*v[0]]
-            s = 0
-            for i in range(3):
-                s = s+normal[i]*verticies[surface[0]][i]
-            if s > 0:
-                normals.append(normal)
-            else:
-                normals.append([-normal[0], -normal[1], -normal[2]])
-            for vertex in surface:
-                vertices_to_surfaces[vertex].append(x-1)  	
-        x = 0
-        vertex_normalized = []
-        for j in range(len(vertices_to_surfaces)):
-            vertex_normalized.append([0, 0, 0])
-            for z in range(len(vertices_to_surfaces[j])):
-                normal = normals[vertices_to_surfaces[j][z]]
-                vertex_normalized[j][0] = vertex_normalized[j][0] + normal[0]
-                vertex_normalized[j][1] = vertex_normalized[j][1] + normal[1]
-                vertex_normalized[j][2] = vertex_normalized[j][2] + normal[2]
-            norm = np.sqrt(vertex_normalized[j][0]**2 + vertex_normalized[j][1]**2 + vertex_normalized[j][2]**2)
-            for z in range(3):
-                vertex_normalized[j][z] = vertex_normalized[j][z]/norm
-
-
-        for surface in surfaces:
-            #print(surface)
-            for vertex in surface:
-                #TODO: something with colors, now it is too far from reality
-                l = sum(opp(verticies[vertex]), lightpos)
-                cos_theta = (vertex_normalized[vertex][0]*l[0] + vertex_normalized[vertex][1]*l[1] + vertex_normalized[vertex][2]*l[2])/(dist(vertex_normalized[vertex], [0.,0.,0.])*dist(l, [0.,0.,0.]))
-                glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, (1., 0., 1., 1) )
-                #glColor4f(.23,.78,.32,100.0);            	
+                glBegin(GL_TRIANGLE_FAN);
+                for vertex in surface:
+                    #TODO: something with colors, now it is too far from reality
+                    l = sum(opp(verticies[vertex]), lightpos) #lighpos - vector to vertex = vector from vertex to lights
+                    cos_theta = (vertex_normalized[vertex][0]*l[0] + vertex_normalized[vertex][1]*l[1] + vertex_normalized[vertex][2]*l[2])/(dist(vertex_normalized[vertex], [0.,0.,0.])*dist(l, [0.,0.,0.]))
+                    glMaterialfv(GL_FRONT_AND_BACK,GL_DIFFUSE, (1., 0., 1., 1) )
+                    #glMaterialfv(GL_FRONT_AND_BACK,GL_SPECULAR, (1., 0., 1., 1) )
+                    glColor4f(.23,.78,.32,100.0);            	
                 
-                #GL_FLAT
-                #print(vertex_normalized[vertex])
-                #print(cos_theta)
-                glNormal3fv(div(vertex_normalized[vertex],1./cos_theta))
-                glVertex3fv(verticies[vertex]) 
-                #glShadeModel(GL_SMOOTH)       	
-        glEnd()
-
+                    #GL_FLAT
+                    #print(vertex_normalized[vertex])
+                    #print(cos_theta)
+                    glNormal3fv(div(vertex_normalized[vertex],1./cos_theta))
+                    glVertex3fv(verticies[vertex]) 
+                glEnd() 
+                glDepthFunc(GL_LESS);
+                    #glShadeModel(GL_SMOOTH)       	
+            #glEnd()
+            
+            #glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
     
 
-        #glPopMatrix()                                               # Возвращаем сохраненное положение "камеры"
+            #glPopMatrix()                                               # Возвращаем сохраненное положение "камеры"
                                             # Выводим все нарисованное в памяти на экран
-        #for i in range(len(centers)):
-        eye = [0., 0., 0.]    
-        xrot = 0.0                          # Величина вращения по оси x = 0
-        yrot = 0.0                          # Величина вращения по оси y = 0
-        ambient = (1.0, 1.0, 1.0, 10)        # Первые три числа - цвет в формате RGB, а последнее - яркость
-        
-        center = centers[iteration]
-        up = directions[iteration]
-        print(center)
-        lightpos = (-50*up[0], -50*up[1], -50*up[2])          # Положение источника освещения по осям xyz
+            #for i in range(len(centers)):
+            eye = [0., 0., 0.]    
+            xrot = 0.0                          # Величина вращения по оси x = 0
+            yrot = 0.0                          # Величина вращения по оси y = 0
+            ambient = (1.0, 1.0, 1.0, 10)        # Первые три числа - цвет в формате RGB, а последнее - яркость
+            
+            center = centers[iteration]
+            up = directions[iteration]
+            print(center)
+            lightpos = (-50*up[0], -50*up[1], -50*up[2])          # Положение источника освещения по осям xyz
+    
+            #lightpos = (-50*up[0]*(-center[0] + eye[0]), -50*up[1]*(-center[1]+ eye[0]), -50*up[2]*(-center[2]+ eye[0]))          # Положение источника освещения по осям xyz
 
-    #lightpos = (-50*up[0]*(-center[0] + eye[0]), -50*up[1]*(-center[1]+ eye[0]), -50*up[2]*(-center[2]+ eye[0]))          # Положение источника освещения по осям xyz
+            #gluLookAt(eye[0] , eye[1], eye[2], center[0], center[1], center[2], up[0], up[1], up[2])
+            #glEnable(GL_DEPTH_TEST);
+            #glDepthFunc(GL_LESS);
+            glutSwapBuffers()   
+            #glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE)       
+            x, y, width, height = glGetIntegerv(GL_VIEWPORT)
+            print("Screenshot viewport:", x, y, width, height)
+            glPixelStorei(GL_PACK_ALIGNMENT, 1)
 
-        gluLookAt(eye[0] , eye[1], eye[2], center[0], center[1], center[2], up[0], up[1], up[2])
-        glutSwapBuffers()   
-          
-        x, y, width, height = glGetIntegerv(GL_VIEWPORT)
-        print("Screenshot viewport:", x, y, width, height)
-        glPixelStorei(GL_PACK_ALIGNMENT, 1)
-
-        data = glReadPixels(x, y, width, height, GL_RGB, GL_UNSIGNED_BYTE)
-
-        image_array = np.fromstring(data, np.uint8)
-        image = image_array.reshape(width, height, 3)
-        scipy.misc.imsave('images/' + filename[7:-4] + '_outfile_' + str(iteration) + '.jpg', image)
+            data = glReadPixels(x, y, width, height, GL_RGB, GL_UNSIGNED_BYTE)
+    
+            image_array = np.fromstring(data, np.uint8)
+            image = image_array.reshape(width, height, 3)
+            scipy.misc.imsave('images/' + filename[7:-4] + '_outfile_' + str(iteration) + '.jpg', image)
     glutDestroyWindow(1)
-    #-------------------------------
+    if True:
+        pass
+        #-------------------------------
 
-    #glPushMatrix()                                              # Сохраняем текущее положение "камеры"
-    #glRotatef(xrot, 1.0, 0.0, 0.0)                              # Вращаем по оси X на величину xrot
-    #glRotatef(yrot, 0.0, 1.0, 0.0)                              # Вращаем по оси Y на величину yrot
-    """
-    # Рисуем ствол елки
-    # Устанавливаем материал: рисовать с 2 сторон, рассеянное освещение, коричневый цвет
-    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, treecolor)
-    glTranslatef(0.0, 0.0, -0.7)                                # Сдвинемся по оси Z на -0.7
-    # Рисуем цилиндр с радиусом 0.1, высотой 0.2
-    # Последние два числа определяют количество полигонов
-    glutSolidCylinder(0.1, 0.2, 20, 20)
-    # Рисуем ветки елки
-    # Устанавливаем материал: рисовать с 2 сторон, рассеянное освещение, зеленый цвет
-    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, greencolor)
-    glTranslatef(0.0, 0.0, 0.2)                                 # Сдвинемся по оси Z на 0.2
-    # Рисуем нижние ветки (конус) с радиусом 0.5, высотой 0.5
-    # Последние два числа определяют количество полигонов
-    glutSolidCone(0.5, 0.5, 20, 20)
-    glTranslatef(0.0, 0.0, 0.3)                                 # Сдвинемся по оси Z на -0.3
-    glutSolidCone(0.4, 0.4, 20, 20)                             # Конус с радиусом 0.4, высотой 0.4
-    glTranslatef(0.0, 0.0, 0.3)                                 # Сдвинемся по оси Z на -0.3
-    glutSolidCone(0.3, 0.3, 20, 20)                             # Конус с радиусом 0.3, высотой 0.3
-
-    glPopMatrix()                                               # Возвращаем сохраненное положение "камеры"
-    glutSwapBuffers()                                           # Выводим все нарисованное в памяти на экран
-    """
+        #glPushMatrix()                                              # Сохраняем текущее положение "камеры"
+        #glRotatef(xrot, 1.0, 0.0, 0.0)                              # Вращаем по оси X на величину xrot
+        #glRotatef(yrot, 0.0, 1.0, 0.0)                              # Вращаем по оси Y на величину yrot
+        """
+        # Рисуем ствол елки
+        # Устанавливаем материал: рисовать с 2 сторон, рассеянное освещение, коричневый цвет
+        glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, treecolor)
+        glTranslatef(0.0, 0.0, -0.7)                                # Сдвинемся по оси Z на -0.7
+        # Рисуем цилиндр с радиусом 0.1, высотой 0.2
+        # Последние два числа определяют количество полигонов
+        glutSolidCylinder(0.1, 0.2, 20, 20)
+        # Рисуем ветки елки
+        # Устанавливаем материал: рисовать с 2 сторон, рассеянное освещение, зеленый цвет
+        glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, greencolor)
+        glTranslatef(0.0, 0.0, 0.2)                                 # Сдвинемся по оси Z на 0.2
+        # Рисуем нижние ветки (конус) с радиусом 0.5, высотой 0.5
+        # Последние два числа определяют количество полигонов
+        glutSolidCone(0.5, 0.5, 20, 20)
+        glTranslatef(0.0, 0.0, 0.3)                                 # Сдвинемся по оси Z на -0.3
+        glutSolidCone(0.4, 0.4, 20, 20)                             # Конус с радиусом 0.4, высотой 0.4
+        glTranslatef(0.0, 0.0, 0.3)                                 # Сдвинемся по оси Z на -0.3
+        glutSolidCone(0.3, 0.3, 20, 20)                             # Конус с радиусом 0.3, высотой 0.3
+    
+        glPopMatrix()                                               # Возвращаем сохраненное положение "камеры"
+        glutSwapBuffers()                                           # Выводим все нарисованное в памяти на экран
+        """
 def runAll(model_name, centers=0, directions=0):
 	print(model_name)
 	global filename
@@ -227,15 +250,17 @@ def runAll(model_name, centers=0, directions=0):
 	# Запускаем основной цикл
 	glutMainLoop()
 
-def execute(fname, centers0, directions0, iteration0):
+def execute(fname, centers0, directions0, iteration0, names0):
 	global filename
 	global centers
 	global directions
 	global iteration
+	global names
 	filename = fname
 	centers = centers0
-	iteration = iteration0
 	directions = directions0
+	iteration = iteration0
+	names = names0
 	runAll(fname)
 
 #centers0, directions0 = determineViewDirections(d = 10)
